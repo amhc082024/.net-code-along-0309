0a0ccccd11b8fafaeba19aed162f9ad5c95a917d
amhc082024 <amhc082024@gmail.com>
2024-09-03 12:37:26 +0200

    v3

--- Program.cs
+++ Program.cs
@@ -5,10 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-    }
-    {
+    
         int myNumber = 1;
         Console.WriteLine(myNumber);
+
+        string greeting = ("Hola");
+        Console.WriteLine(greeting);
+
+        double MyDouble = 1.11;
+        Console.WriteLine(MyDouble);
+
+        char MyChar = 'C';
+        Console.WriteLine(MyChar);
+
+        int[] MyNumSeries = [10, 20, 40, 90];
+        Console.WriteLine(MyNumSeries);
+        for(int i=0; i<MyNumSeries.Length; i=i+1)
+        {
+            Console.WriteLine(MyNumSeries[i]);
+
+        }
+
+        foreach (int item in MyNumSeries)
+        {
+            Console.WriteLine(item);
+        }
+
+
+
+
+
 
         
     }
